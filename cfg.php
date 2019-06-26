a422562bad9140a29558ebe6a12fa9b93904aa42
MRT-REX <48020975+MRT-REX@users.noreply.github.com>
2019-06-26 17:03:57 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="";
+
+//user_gaid wajib isi
+$user_gaid= "9116cc5a-7b9c-4bb0-89f2-5e20f2c535d7";
+//user_token wajib di isi //
+$user_token="AbxlFkxkyjzTgFCL";
+
+//user_id wajib di isi //
+$user_id="Facebook_143622970126138";
+
+// invite_code wajib di isi //
+$invite_code="FemYtu2FW";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDM2MjI5NzAxMjYxMzgiLCJleHAiOiIxNTY0MDQyNjA1NDE4In0.3RI9N4MkH-lWbwBcGbTwlQVM9SM_6-7MC6ZkGhUVZe8";
+
+
+
+?>
